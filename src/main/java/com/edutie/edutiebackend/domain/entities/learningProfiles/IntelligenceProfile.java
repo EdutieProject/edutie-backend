e9065a52137a109d11c23cc5e691e19840dcbc57
sukerberk1 <szymon.l3232@gmail.com>
2023-10-18 20:28:46 +0200

    Add default constructors for SkillsProfile and IntelligenceProfile

--- src/main/java/com/edutie/edutiebackend/domain/entities/learningProfiles/IntelligenceProfile.java
+++ src/main/java/com/edutie/edutiebackend/domain/entities/learningProfiles/IntelligenceProfile.java
@@ -8,6 +8,16 @@ import java.util.HashMap;
 public class IntelligenceProfile implements ILearningProfile {
     private final HashMap<Intelligence, Double> intelligencePoints = new HashMap<>();
 
+    /**
+     * Default constructor. Should be changed in the future to utilize database values.
+     */
+    public IntelligenceProfile() {
+        for (Intelligence intelligence :
+                Intelligence.values()) {
+            intelligencePoints.put(intelligence, 0.0);
+        }
+    }
+
     @Override
     public void adjust(byte learningResult) {
         // perform adjusting
--- src/main/java/com/edutie/edutiebackend/domain/entities/learningProfiles/SkillsProfile.java
+++ src/main/java/com/edutie/edutiebackend/domain/entities/learningProfiles/SkillsProfile.java
@@ -8,6 +8,15 @@ import java.util.HashMap;
 public class SkillsProfile implements ILearningProfile {
     private final HashMap<Skill, Double> skillPoints = new HashMap<>();
 
+    /**
+     * Default constructor. Should be changed in the future to utilize database values.
+     */
+    public SkillsProfile() {
+        for (Skill skill: Skill.values()) {
+            skillPoints.put(skill, 0.0);
+        }
+    }
+
     @Override
     public void adjust(byte learningResult) {
         // perform adjusting
