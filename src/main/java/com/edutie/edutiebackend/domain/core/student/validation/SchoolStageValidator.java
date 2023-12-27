ed56f4e127181435cd32c993d2d74d91015bcf56
Minio <oliwexminio@gmail.com>
2023-12-27 09:18:42 +0100

    Popraw według mojej koncepcji te statyczne metody i zmienne prywatne

--- src/main/java/com/edutie/edutiebackend/domain/core/student/validation/SchoolStageValidator.java
+++ src/main/java/com/edutie/edutiebackend/domain/core/student/validation/SchoolStageValidator.java
@@ -2,6 +2,9 @@ package com.edutie.edutiebackend.domain.core.student.validation;
 
 import com.edutie.edutiebackend.domain.core.student.exceptions.InvalidSchoolStageException;
 import com.edutie.edutiebackend.domain.core.student.valueobjects.SchoolStage;
+
+import org.springframework.data.domain.Range;
+
 import com.edutie.edutiebackend.domain.core.student.enums.SchoolType;
 
 /**
@@ -9,39 +12,43 @@ import com.edutie.edutiebackend.domain.core.student.enums.SchoolType;
  * rules defining whether school stage is valid or not.
  */
 public class SchoolStageValidator {
-    public static boolean isValid(SchoolStage schoolStage) throws InvalidSchoolStageException {
-        if(
-                HighSchoolValidation(schoolStage) || TechnicalHighSchoolValidation(schoolStage)  || TertiarySchoolValidation(schoolStage)
-        )
+
+    private SchoolStage schoolStage;
+
+    public SchoolStageValidator(SchoolStage schoolStage)
+    {
+        this.schoolStage=schoolStage;
+    }
+    private static boolean isValidCondition() {
+        return (HighSchoolValidation(this.schoolStage) || TechnicalHighSchoolValidation(this.schoolStage)  || TertiarySchoolValidation(this.schoolStage));
+    }
+    private static boolean isValidRange(int left,int right,var gradeNo) {
+        Range<int> range=new Range().leftOpen(left, right);
+        var validRange=range.contains(gradeNo);
+        return validRange;
+    }
+    public static boolean isValid() throws InvalidSchoolStageException {
+        if(this.isValidCondition())
             return true;
         else
             throw new InvalidSchoolStageException();
     }
 
-    private static boolean HighSchoolValidation(SchoolStage schoolStage)
+    private static boolean HighSchoolValidation()
     {
-        var gradeNo = schoolStage.gradeNumber();
-        return (schoolStage.schoolType() == SchoolType.HighSchool
-                && gradeNo > 0
-                && gradeNo <= 4
-        );
+        var gradeNo = (this.schoolStage).gradeNumber();
+        return (((this.schoolStage).schoolType() == SchoolType.HighSchool) && (this.isValidRange(0,4,gradeNo)));
     }
 
-    private static boolean TechnicalHighSchoolValidation(SchoolStage schoolStage)
+    private static boolean TechnicalHighSchoolValidation()
     {
-        var gradeNo = schoolStage.gradeNumber();
-        return (schoolStage.schoolType() == SchoolType.TechnicalHighSchool
-                && gradeNo > 0
-                && gradeNo <= 5
-        );
+        var gradeNo = (this.schoolStage).gradeNumber();
+        return (((this.schoolStage).schoolType() == SchoolType.TechnicalHighSchool) && (this.isValidRange(0,3,gradeNo)));
     }
 
-    private static boolean TertiarySchoolValidation(SchoolStage schoolStage)
+    private static boolean TertiarySchoolValidation()
     {
-        var gradeNo = schoolStage.gradeNumber();
-        return (schoolStage.schoolType() == SchoolType.TertiarySchool
-                && gradeNo > 0
-                && gradeNo <= 3
-        );
+        var gradeNo = (this.schoolStage).gradeNumber();
+        return (((this.schoolStage).schoolType() == SchoolType.TertiarySchool) && (this.isValidRange(0,5,gradeNo)));
     }
 }
