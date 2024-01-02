5c4da6af5f172e93302e49d148d47ec4e7484d14
sukerberk1 <szymon.l3232@gmail.com>
2024-01-02 21:07:00 +0100

    Unlock persistence

--- src/main/java/com/edutie/edutiebackend/EdutieBackendApplication.java
+++ src/main/java/com/edutie/edutiebackend/EdutieBackendApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// exclude DataSourceAutoConfiguration - this disables database configuration and needs to
-// be deleted once we configure database connection
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+@SpringBootApplication
 public class EdutieBackendApplication {
 
     public static void main(String[] args) {
