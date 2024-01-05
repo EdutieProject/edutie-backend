8faf5b8ac54986ec91fece2121fb5b2374ac628a
sukerberk1 <szymon.l3232@gmail.com>
2024-01-05 21:02:54 +0100

    Refine OptimizationStrategy + slight changes

---
+++ src/main/java/com/edutie/edutiebackend/domain/core/optimizationstrategies/IntelligenceOptimizationStrategy.java
@@ -0,0 +1,13 @@
+package com.edutie.edutiebackend.domain.core.optimizationstrategies;
+
+import com.edutie.edutiebackend.domain.core.common.studenttraits.Ability;
+import com.edutie.edutiebackend.domain.core.optimizationstrategies.base.OptimizationStrategy;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Entity
+public class IntelligenceOptimizationStrategy extends OptimizationStrategy<Ability> {
+}
