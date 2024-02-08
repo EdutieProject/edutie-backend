648d54d93608fa754a6b36cbb1e5f8b4bfabb06c
sukerberk1 <szymon.l3232@gmail.com>
2024-02-08 15:38:01 +0100

    #31 - validation refactoring prep

--- src/main/java/validation/RuleEnforcer.java
+++ src/main/java/validation/RuleEnforcer.java
@@ -1,10 +1,9 @@
 package validation;
 
-import validation.rules.CompoundRule;
 import validation.rules.Rule;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Functional class providing necessary behaviour for rule validation
@@ -13,28 +12,18 @@ import java.util.ArrayList;
 public final class RuleEnforcer {
     /**
      * Validate a ruleClass of given object
-     * @param ruleClass class of the ruleClass
+     *
+     * @param ruleClass  class of the ruleClass
      * @param ruleObject validated object
+     * @param <U>        type of ruleClass class
+     * @param <T>        type of object to validate
      * @return Result of type T
-     * @param <U> type of ruleClass class
-     * @param <T> type of object to validate
      */
     public static <U extends Rule<T>, T> Result validate(Class<U> ruleClass, T ruleObject) {
         List<Error> errors;
         try {
             U rule = ruleClass.getConstructor().newInstance();
             errors = new ArrayList<>(rule.check(ruleObject));
-        } catch (Exception exception) {
-            throw new RuntimeException(exception);
-        }
-        return Result.fromErrorList(errors);
-    }
-
-    public static <U extends CompoundRule<T, S>, T, S> Result validate(Class<U> ruleClass, T first, S second) {
-        List<Error> errors;
-        try {
-            U rule = ruleClass.getConstructor().newInstance();
-            errors = new ArrayList<>(rule.check(first, second));
         } catch (Exception exception) {
             throw new RuntimeException(exception);
         }
--- src/main/java/validation/rules/CompoundRule.java
+++
@@ -1,9 +0,0 @@
-package validation.rules;
-
-import validation.Error;
-
-import java.util.List;
-
-public interface CompoundRule<T, U> {
-    List<Error> check(T first, U second);
-}
