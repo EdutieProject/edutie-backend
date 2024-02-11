b2cfbec50f018cfecae041cc732e4f242860c2cc
sukerberk1 <szymon.l3232@gmail.com>
2024-02-11 20:47:16 +0100

    Recommended constructors for Skill and LearningResource

--- src/main/java/com/edutie/backend/domain/personalization/learningresource/LearningResource.java
+++ src/main/java/com/edutie/backend/domain/personalization/learningresource/LearningResource.java
@@ -2,19 +2,15 @@ package com.edutie.backend.domain.personalization.learningresource;
 
 import com.edutie.backend.domain.common.Utilities;
 import com.edutie.backend.domain.common.base.AuditableEntityBase;
+import com.edutie.backend.domain.common.identities.UserId;
+import com.edutie.backend.domain.learner.student.Student;
 import com.edutie.backend.domain.personalization.learningresource.identities.LearningResourceId;
 import com.edutie.backend.domain.personalization.optimizationstrategies.AbilityOptimizationStrategy;
 import com.edutie.backend.domain.personalization.optimizationstrategies.IntelligenceOptimizationStrategy;
 import com.edutie.backend.domain.personalization.optimizationstrategies.base.OptimizationStrategy;
 import com.edutie.backend.domain.studyprogram.lessonsegment.LessonSegment;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -38,14 +34,27 @@ public class LearningResource extends AuditableEntityBase<LearningResourceId> {
     @ManyToMany
     private final Set<AbilityOptimizationStrategy> abilityOptimizationStrategies = new HashSet<>();
     @ManyToOne(targetEntity = LessonSegment.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "lesson_segment_id")
+    @Setter(AccessLevel.PRIVATE)
     private LessonSegment lessonSegment;
+    @ManyToOne(targetEntity = Student.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "student_id")
+    @Setter(AccessLevel.PRIVATE)
+    private Student student;
 
     /**
-     * Recommended constructor which associates the resource with concrete
-     * lesson segment.
+     * Recommended constructor associating learning resource with a student (creation invoker) and a lesson segment.
+     * @param student student profile reference
+     * @param lessonSegment lesson segment reference
+     * @return Learning Resource
      */
-    public LearningResource(LessonSegment lessonSegment) {
-        this.lessonSegment = lessonSegment;
+    public static LearningResource create(Student student, LessonSegment lessonSegment) {
+        LearningResource learningResource = new LearningResource();
+        learningResource.setId(new LearningResourceId());
+        learningResource.setCreatedBy(student.getCreatedBy());
+        learningResource.setStudent(student);
+        learningResource.setLessonSegment(lessonSegment);
+        return learningResource;
     }
 
     public Set<OptimizationStrategy<?>> getAllOptimizationStrategies() {
