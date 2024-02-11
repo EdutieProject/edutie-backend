1cd3a6a4881634ea9677b9f4b8977c47beb30985
sukerberk1 <szymon.l3232@gmail.com>
2024-02-11 20:45:14 +0100

    Recommended constructors for subdomain Roles

--- src/main/java/com/edutie/backend/domain/psychology/psychologist/Psychologist.java
+++ src/main/java/com/edutie/backend/domain/psychology/psychologist/Psychologist.java
@@ -1,9 +1,16 @@
 package com.edutie.backend.domain.psychology.psychologist;
 
 import com.edutie.backend.domain.common.base.AuditableEntityBase;
+import com.edutie.backend.domain.common.identities.UserId;
 import com.edutie.backend.domain.psychology.psychologist.identities.PsychologistId;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Psychologist extends AuditableEntityBase<PsychologistId> {
+    public static Psychologist create(UserId userId) {
+        Psychologist psychologist = new Psychologist();
+        psychologist.setId(new PsychologistId());
+        psychologist.setCreatedBy(userId);
+        return psychologist;
+    }
 }
