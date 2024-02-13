7c6972d4dc6aa328b83d343f9878797e5c181d6f
sukerberk1 <szymon.l3232@gmail.com>
2024-02-13 13:41:55 +0100

    Recommended constructors in OptimizationStrategy

--- src/main/java/com/edutie/backend/domain/personalization/optimizationstrategies/base/OptimizationStrategy.java
+++ src/main/java/com/edutie/backend/domain/personalization/optimizationstrategies/base/OptimizationStrategy.java
@@ -2,6 +2,11 @@ package com.edutie.backend.domain.personalization.optimizationstrategies.base;
 
 import com.edutie.backend.domain.common.base.AuditableEntityBase;
 import com.edutie.backend.domain.common.generationprompt.PromptFragment;
+import com.edutie.backend.domain.common.identities.UserId;
+import com.edutie.backend.domain.common.studenttraits.Ability;
+import com.edutie.backend.domain.common.studenttraits.Intelligence;
+import com.edutie.backend.domain.personalization.optimizationstrategies.AbilityOptimizationStrategy;
+import com.edutie.backend.domain.personalization.optimizationstrategies.IntelligenceOptimizationStrategy;
 import com.edutie.backend.domain.personalization.optimizationstrategies.identities.OptimizationStrategyId;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
@@ -26,4 +31,20 @@ public abstract class OptimizationStrategy<TTrait extends Enum<TTrait>> extends 
 
     public abstract TTrait getTrait();
     public abstract void setTrait(TTrait trait);
+
+    private static AbilityOptimizationStrategy create(UserId userId, Ability ability) {
+        AbilityOptimizationStrategy abilityOptimizationStrategy = new AbilityOptimizationStrategy();
+        abilityOptimizationStrategy.setId(new OptimizationStrategyId());
+        abilityOptimizationStrategy.setCreatedBy(userId);
+        abilityOptimizationStrategy.setTrait(ability);
+        return abilityOptimizationStrategy;
+    }
+
+    private static IntelligenceOptimizationStrategy create(UserId userId, Intelligence intelligence) {
+        IntelligenceOptimizationStrategy abilityOptimizationStrategy = new IntelligenceOptimizationStrategy();
+        abilityOptimizationStrategy.setId(new OptimizationStrategyId());
+        abilityOptimizationStrategy.setCreatedBy(userId);
+        abilityOptimizationStrategy.setTrait(intelligence);
+        return abilityOptimizationStrategy;
+    }
 }
