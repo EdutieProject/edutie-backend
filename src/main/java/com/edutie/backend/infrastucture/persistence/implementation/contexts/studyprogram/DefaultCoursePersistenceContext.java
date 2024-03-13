77b32bf379db45eda67946a120e9b2d8309fe3b8
cezary <01178710@pw.edu.pl>
2024-03-13 12:23:48 +0100

    Begin of the work

---
+++ src/main/java/com/edutie/backend/infrastucture/persistence/implementation/contexts/studyprogram/DefaultCoursePersistenceContext.java
@@ -0,0 +1,46 @@
+package com.edutie.backend.infrastucture.persistence.implementation.contexts.studyprogram;
+
+import com.edutie.backend.domain.studyprogram.course.Course;
+import com.edutie.backend.domain.studyprogram.course.identities.CourseId;
+import com.edutie.backend.domain.studyprogram.science.identities.ScienceId;
+import com.edutie.backend.infrastucture.persistence.contexts.studyprogram.CoursePersistenceContext;
+import org.springframework.stereotype.Component;
+import validation.Result;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class DefaultCoursePersistenceContext implements CoursePersistenceContext {
+
+    @Override
+    public Optional<Course> getById(CourseId courseId) {
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Result save(Course entity) {
+        return null;
+    }
+
+    @Override
+    public Result deleteById(CourseId courseId) {
+        return null;
+    }
+
+    @Override
+    public List<Course> getAllOfScienceId(ScienceId scienceId) {
+        return null;
+    }
+
+    @Override
+    public List<Course> getAllAccessibleOfScienceId(ScienceId scienceId) {
+        return null;
+    }
+
+    @Override
+    public List<Course> getAllInaccessibleOfScienceId(ScienceId scienceId) {
+        return null;
+    }
+}
