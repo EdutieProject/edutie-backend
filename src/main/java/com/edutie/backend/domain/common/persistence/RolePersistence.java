ef288a6fbc9b867bd8b5f6405fb82dcfec11c0e4
sukerberk1 <szymon.l3232@gmail.com>
2024-04-13 13:31:19 +0200

    Persistence - add role persistence

---
+++ src/main/java/com/edutie/backend/domain/common/persistence/RolePersistence.java
@@ -0,0 +1,34 @@
+package com.edutie.backend.domain.common.persistence;
+
+import com.edutie.backend.domain.common.base.EntityBase;
+import com.edutie.backend.domain.common.identities.UserId;
+import validation.Result;
+
+import java.io.Serializable;
+
+public interface RolePersistence<TEntity extends EntityBase<TId>, TId extends Serializable> {
+    /**
+     * Retrieves role profile for the given user. The presence of the role profile should be checked
+     * using authorization before using this function.
+     * @param userId id of a user
+     * @return Role profile of a user
+     */
+    TEntity getByUserId(UserId userId);
+
+    /**
+     * Persists the provided role profile into the database. If it is already present,
+     * updates its state to the provided object's state. Returns result indicating whether
+     * the operation was successful or not
+     * @return Result object
+     */
+    Result save(TEntity entity);
+
+    /**
+     * Removes the role for the user of specified id from the database. Success is returned even
+     * when the role is not present at first in the database. Failure is returned only when something goes
+     * wrong
+     * @param userId entity id
+     * @return Result object
+     */
+    Result removeForUser(UserId userId);
+}
--- src/main/java/com/edutie/backend/domain/education/educator/persistence/EducatorPersistence.java
+++ src/main/java/com/edutie/backend/domain/education/educator/persistence/EducatorPersistence.java
@@ -1,17 +1,8 @@
 package com.edutie.backend.domain.education.educator.persistence;
 
-import com.edutie.backend.domain.common.identities.UserId;
-import com.edutie.backend.domain.common.persistence.PersistenceBase;
+import com.edutie.backend.domain.common.persistence.RolePersistence;
 import com.edutie.backend.domain.education.educator.Educator;
 import com.edutie.backend.domain.education.educator.identities.EducatorId;
-import validation.WrapperResult;
 
-public interface EducatorPersistence extends PersistenceBase<Educator, EducatorId> {
-    /**
-     * Retrieves the creator associated with given user
-     *
-     * @param userId user id
-     * @return Optional Creator
-     */
-    WrapperResult<Educator> getByUserId(UserId userId);
+public interface EducatorPersistence extends RolePersistence<Educator, EducatorId> {
 }
