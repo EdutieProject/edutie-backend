73b5fda6b310089d83e02362a0b8c4712852062c
sukerberk1 <szymon.l3232@gmail.com>
2024-04-21 14:41:27 +0200

    Add SciencePersistenceImplementation

--- src/main/java/com/edutie/backend/infrastucture/persistence/implementation/persistence/studyprogram/SciencePersistenceImplementation.java
+++ src/main/java/com/edutie/backend/infrastucture/persistence/implementation/persistence/studyprogram/SciencePersistenceImplementation.java
@@ -3,18 +3,22 @@ package com.edutie.backend.infrastucture.persistence.implementation.persistence.
 import com.edutie.backend.domain.studyprogram.science.Science;
 import com.edutie.backend.domain.studyprogram.science.identities.ScienceId;
 import com.edutie.backend.domain.studyprogram.science.persistence.SciencePersistence;
+import com.edutie.backend.infrastucture.persistence.implementation.jpa.repositories.ScienceRepository;
+import com.edutie.backend.infrastucture.persistence.implementation.persistence.common.PersistenceError;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import validation.Result;
 import validation.WrapperResult;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class SciencePersistenceImplementation implements SciencePersistence {
+    private final ScienceRepository scienceRepository;
     /**
-     * Retrieves the entity using its identifier. Entity is wrapped in optional, therefore
+     * Retrieves the entity using its identifier. Entity is wrapped, therefore
      * it might not be present based on the identifier presence in the database.
      *
      * @param scienceId entity id
@@ -22,7 +26,13 @@ public class SciencePersistenceImplementation implements SciencePersistence {
      */
     @Override
     public WrapperResult<Science> getById(ScienceId scienceId) {
-        return null;
+        try {
+            Optional<Science> scienceOptional = scienceRepository.findById(scienceId);
+            return scienceOptional.map(WrapperResult::successWrapper)
+                    .orElseGet(() -> WrapperResult.failureWrapper(PersistenceError.notFound(Science.class)));
+        } catch (Exception exception) {
+            return WrapperResult.failureWrapper(PersistenceError.exceptionEncountered(exception));
+        }
     }
 
     /**
@@ -30,12 +40,17 @@ public class SciencePersistenceImplementation implements SciencePersistence {
      * updates its state to the provided object's state. Returns result indicating whether
      * the operation was successful or not
      *
-     * @param entity
+     * @param entity entity
      * @return Result object
      */
     @Override
     public Result save(Science entity) {
-        return null;
+        try {
+            scienceRepository.saveAndFlush(entity);
+            return Result.success();
+        } catch (Exception exception) {
+            return Result.failure(PersistenceError.exceptionEncountered(exception));
+        }
     }
 
     /**
@@ -47,7 +62,12 @@ public class SciencePersistenceImplementation implements SciencePersistence {
      */
     @Override
     public Result removeById(ScienceId scienceId) {
-        return null;
+        try {
+            scienceRepository.deleteById(scienceId);
+            return Result.success();
+        } catch (Exception exception) {
+            return Result.failure(PersistenceError.exceptionEncountered(exception));
+        }
     }
 
     /**
@@ -56,7 +76,12 @@ public class SciencePersistenceImplementation implements SciencePersistence {
      * @return Science list
      */
     @Override
-    public List<Science> getAll() {
-        return null;
+    public WrapperResult<List<Science>> getAll() {
+        try {
+            List<Science> sciences = scienceRepository.findAll();
+            return WrapperResult.successWrapper(sciences);
+        } catch (Exception exception) {
+            return WrapperResult.failureWrapper(PersistenceError.exceptionEncountered(exception));
+        }
     }
 }
