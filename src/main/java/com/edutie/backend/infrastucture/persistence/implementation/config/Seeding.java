086b68d4cc782cf62bd99721f2e172728fe30fd4
sukerberk1 <szymon.l3232@gmail.com>
2024-04-30 15:33:47 +0200

    Prepare seeding class for db seeding feature

---
+++ src/main/java/com/edutie/backend/infrastucture/persistence/implementation/config/Seeding.java
@@ -0,0 +1,11 @@
+package com.edutie.backend.infrastucture.persistence.implementation.config;
+
+public class Seeding {
+
+    /**
+     * Seed database with sample study program
+     */
+    public void seedStudyProgram() {
+        //TODO
+    }
+}
