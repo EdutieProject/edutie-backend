0e48480bf52b5838ca85e0e4e88e0c882eb4bec8
sukerberk1 <szymon.l3232@gmail.com>
2024-05-24 21:52:35 +0200

    Course serialization setup

---
+++ src/main/java/com/edutie/backend/api/serialization/IdOnlySerializer.java
@@ -0,0 +1,26 @@
+package com.edutie.backend.api.serialization;
+
+import com.edutie.backend.domain.common.base.EntityBase;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+
+public class IdOnlySerializer<T extends EntityBase<?>> extends StdSerializer<T> {
+    public IdOnlySerializer() {
+        this(null);
+    }
+    protected IdOnlySerializer(Class<T> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(T t, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        //TODO: zalepione na taśmę, do naprawy
+        String idRepresentation = t.getId().toString();
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeStringField("id", idRepresentation.substring(idRepresentation.length()-37, idRepresentation.length()-1));
+        jsonGenerator.writeEndObject();
+    }
+}
