f3c7caa6fb7c5ec3a4d1d9a62d6f140214189807
sukerberk1 <szymon.l3232@gmail.com>
2024-06-18 22:24:18 +0200

    Restore keycloak auth

--- src/main/java/com/edutie/backend/EdutieBackendApplication.java
+++ src/main/java/com/edutie/backend/EdutieBackendApplication.java
@@ -4,10 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Edutie API", summary = "Edutie backend app", version = "0.1"))
 public class EdutieBackendApplication {
 
