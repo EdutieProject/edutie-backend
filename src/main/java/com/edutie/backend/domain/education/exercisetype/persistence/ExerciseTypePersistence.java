0ff3622c353b9b39bd7a1a65d61f348af8a46efb
sukerberk1 <szymon.l3232@gmail.com>
2024-06-20 16:28:12 +0200

    Refactor persistence implementations - roles part 3 ExerciseTypePersistence

--- src/main/java/com/edutie/backend/domain/education/exercisetype/persistence/ExerciseTypePersistence.java
+++ src/main/java/com/edutie/backend/domain/education/exercisetype/persistence/ExerciseTypePersistence.java
@@ -1,9 +1,8 @@
 package com.edutie.backend.domain.education.exercisetype.persistence;
 
-import com.edutie.backend.domain.administration.UserId;
+import com.edutie.backend.domain.common.persistence.Persistence;
 import com.edutie.backend.domain.education.exercisetype.ExerciseType;
 import com.edutie.backend.domain.education.exercisetype.identities.ExerciseTypeId;
-import com.edutie.backend.domain.common.persistence.Persistence;
 import validation.WrapperResult;
 
 import java.util.List;
@@ -11,14 +10,8 @@ import java.util.List;
 public interface ExerciseTypePersistence extends Persistence<ExerciseType, ExerciseTypeId> {
     /**
      * Retrieve all exercise types
+     *
      * @return Wrapper result of desired list
      */
     WrapperResult<List<ExerciseType>> getAll();
-
-    /**
-     * Retrieve all exercise types associated with given educator
-     * @param userId user id
-     * @return Wrapper result of desired list
-     */
-    WrapperResult<List<ExerciseType>> getAllOfUserId(UserId userId);
 }
