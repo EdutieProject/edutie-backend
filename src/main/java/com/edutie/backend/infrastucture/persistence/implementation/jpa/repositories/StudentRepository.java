8958fb647b21d566f531bd3850990f1a6fbe699a
sukerberk1 <szymon.l3232@gmail.com>
2024-06-20 16:26:52 +0200

    Refactor persistence implementations - roles part 2 generic role repo

--- src/main/java/com/edutie/backend/infrastucture/persistence/implementation/jpa/repositories/StudentRepository.java
+++ src/main/java/com/edutie/backend/infrastucture/persistence/implementation/jpa/repositories/StudentRepository.java
@@ -1,12 +1,8 @@
 package com.edutie.backend.infrastucture.persistence.implementation.jpa.repositories;
 
-import com.edutie.backend.domain.administration.UserId;
 import com.edutie.backend.domain.learner.student.Student;
 import com.edutie.backend.domain.learner.student.identities.StudentId;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.edutie.backend.infrastucture.persistence.implementation.jpa.repositories.common.RoleRepository;
 
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student, StudentId> {
-    List<Student> findStudentsByOwnerUserId(UserId userId);
+public interface StudentRepository extends RoleRepository<Student, StudentId> {
 }
