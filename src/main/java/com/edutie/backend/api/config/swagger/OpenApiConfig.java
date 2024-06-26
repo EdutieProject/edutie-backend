e2fb589bf5342477c73c1b811e04403996eb0ef7
sukerberk1 <szymon.l3232@gmail.com>
2024-06-27 00:02:30 +0200

    Add swagger jwt auth

---
+++ src/main/java/com/edutie/backend/api/config/swagger/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.edutie.backend.api.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                //defining security scheme
+                .components(new Components()
+                        .addSecuritySchemes("JWTAuthentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"))
+                )
+                //setting global security
+                .security(List.of(new SecurityRequirement().addList("JWTAuthentication")));
+    }
+}
