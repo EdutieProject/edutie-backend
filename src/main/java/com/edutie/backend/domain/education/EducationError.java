04e9e446963d8b71f6f76cc83bcf6ec3abb41d30
sukerberk1 <szymon.l3232@gmail.com>
2024-07-06 17:54:13 +0200

    Remove redundant errors

--- src/main/java/com/edutie/backend/domain/education/EducationError.java
+++ src/main/java/com/edutie/backend/domain/education/EducationError.java
@@ -14,18 +14,4 @@ public class EducationError {
                 "EDUCATOR-TYPE-403",
                 "To perform this operation educator must be of higher rank (type)");
     }
-
-    public static Error subRequirementsInvalidIndex() {
-        return new Error(
-                "LEARNING-SUB-REQUIREMENT-400",
-                "Operation could not be performed because the index of the provided sub requirement is invalid."
-        );
-    }
-
-    public static Error reportTemplateParagraphInvalidIndex() {
-        return new Error(
-                "REPORT-TEMPLATE-PARAGRAPH-400",
-                "Operation could not be performed because the index of the provided report template paragraph is invalid."
-        );
-    }
 }
