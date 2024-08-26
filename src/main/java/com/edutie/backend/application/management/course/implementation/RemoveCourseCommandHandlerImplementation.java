70f00d2cd61cdc5c926e79a263ff4c962c673835
sukerberk1 <szymon.l3232@gmail.com>
2024-08-26 12:11:15 +0200

    Add deep removal feature in remove course flow

--- src/main/java/com/edutie/backend/application/management/course/implementation/RemoveCourseCommandHandlerImplementation.java
+++ src/main/java/com/edutie/backend/application/management/course/implementation/RemoveCourseCommandHandlerImplementation.java
@@ -3,36 +3,35 @@ package com.edutie.backend.application.management.course.implementation;
 import com.edutie.backend.application.common.HandlerBase;
 import com.edutie.backend.application.management.course.RemoveCourseCommandHandler;
 import com.edutie.backend.application.management.course.commands.RemoveCourseCommand;
-import com.edutie.backend.domain.education.educator.Educator;
 import com.edutie.backend.domain.education.EducationError;
+import com.edutie.backend.domain.education.educator.Educator;
 import com.edutie.backend.domain.education.educator.persistence.EducatorPersistence;
 import com.edutie.backend.domain.studyprogram.course.Course;
 import com.edutie.backend.domain.studyprogram.course.persistence.CoursePersistence;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import validation.Result;
-import validation.WrapperResult;
 
 @Component
 @RequiredArgsConstructor
 public class RemoveCourseCommandHandlerImplementation extends HandlerBase implements RemoveCourseCommandHandler {
     private final CoursePersistence coursePersistence;
     private final EducatorPersistence educatorPersistence;
+
     @Override
     public Result handle(RemoveCourseCommand command) {
         LOGGER.info("Removing course of id {}", command.courseId().identifierValue());
         Educator educator = educatorPersistence.getByAuthorizedUserId(command.educatorUserId());
-        WrapperResult<Course> courseWrapperResult = coursePersistence.getById(command.courseId());
-        if (courseWrapperResult.isFailure()) {
-            LOGGER.info("Persistence error occurred. Error: " + courseWrapperResult.getError().toString());
-            return courseWrapperResult;
-        }
-        Course course = courseWrapperResult.getValue();
+        Course course = coursePersistence.getById(command.courseId()).getValue();
         if (!educator.isAuthorOf(course)) {
             LOGGER.info("Educator does not have sufficient permissions to modify this course");
             return Result.failure(EducationError.educatorMustBeAuthorError(Course.class));
         }
-        coursePersistence.remove(course);
+        if (command.removeLessons()) {
+            coursePersistence.deepRemove(course).throwIfFailure();
+        } else {
+            coursePersistence.remove(course).throwIfFailure();
+        }
         LOGGER.info("Course removed successfully");
         return coursePersistence.remove(course);
     }
--- src/main/java/com/edutie/backend/domain/studyprogram/course/persistence/CoursePersistence.java
+++ src/main/java/com/edutie/backend/domain/studyprogram/course/persistence/CoursePersistence.java
@@ -5,32 +5,41 @@ import com.edutie.backend.domain.education.educator.identities.EducatorId;
 import com.edutie.backend.domain.studyprogram.course.Course;
 import com.edutie.backend.domain.studyprogram.course.identities.CourseId;
 import com.edutie.backend.domain.studyprogram.science.identities.ScienceId;
+import validation.Result;
 import validation.WrapperResult;
 
 import java.util.List;
 
 public interface CoursePersistence extends Persistence<Course, CourseId> {
-	/**
-	 * Retrieve all courses associated with given science
-	 *
-	 * @param scienceId science id
-	 * @return Wrapper result of the desired list
-	 */
-	WrapperResult<List<Course>> getAllOfScienceId(ScienceId scienceId);
+    /**
+     * Retrieve all courses associated with given science
+     *
+     * @param scienceId science id
+     * @return Wrapper result of the desired list
+     */
+    WrapperResult<List<Course>> getAllOfScienceId(ScienceId scienceId);
 
-	/**
-	 * Retrieve all accessible courses associated with given science
-	 *
-	 * @param scienceId science id
-	 * @return Wrapper result of the desired list
-	 */
-	WrapperResult<List<Course>> getAllAccessibleOfScienceId(ScienceId scienceId);
+    /**
+     * Retrieve all accessible courses associated with given science
+     *
+     * @param scienceId science id
+     * @return Wrapper result of the desired list
+     */
+    WrapperResult<List<Course>> getAllAccessibleOfScienceId(ScienceId scienceId);
 
-	/**
-	 * Retrieve all courses created by given educator
-	 *
-	 * @param educatorId educator id
-	 * @return Wrapper result of the desired list
-	 */
-	WrapperResult<List<Course>> getAllOfEducatorId(EducatorId educatorId);
+    /**
+     * Retrieve all courses created by given educator
+     *
+     * @param educatorId educator id
+     * @return Wrapper result of the desired list
+     */
+    WrapperResult<List<Course>> getAllOfEducatorId(EducatorId educatorId);
+
+    /**
+     * Removes the course together with the underlying lessons & segments
+     *
+     * @param course course to be removed
+     * @return Result object
+     */
+    Result deepRemove(Course course);
 }
