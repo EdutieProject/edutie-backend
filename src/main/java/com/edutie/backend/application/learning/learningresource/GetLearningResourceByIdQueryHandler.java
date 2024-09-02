87f308439491fc2075b48d8e04ed7ccd761cd7a1
sukerberk1 <szymon.l3232@gmail.com>
2024-09-02 15:21:48 +0200

    Add port

---
+++ src/main/java/com/edutie/backend/application/learning/learningresource/GetLearningResourceByIdQueryHandler.java
@@ -0,0 +1,10 @@
+package com.edutie.backend.application.learning.learningresource;
+
+import com.edutie.backend.application.common.UseCaseHandler;
+import com.edutie.backend.application.learning.learningresource.queries.GetLearningResourceByIdQuery;
+import com.edutie.backend.domain.personalization.learningresource.LearningResource;
+import validation.WrapperResult;
+
+public interface GetLearningResourceByIdQueryHandler
+        extends UseCaseHandler<WrapperResult<LearningResource>, GetLearningResourceByIdQuery> {
+}
---
+++ src/main/java/com/edutie/backend/application/learning/learningresource/queries/GetLearningResourceByIdQuery.java
@@ -0,0 +1,26 @@
+package com.edutie.backend.application.learning.learningresource.queries;
+
+import com.edutie.backend.application.common.actions.StudentAction;
+import com.edutie.backend.domain.personalization.learningresource.identities.LearningResourceId;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(fluent = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public final class GetLearningResourceByIdQuery extends StudentAction<GetLearningResourceByIdQuery> {
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private @NonNull LearningResourceId learningResourceId;
+
+    @Override
+    protected GetLearningResourceByIdQuery getThis() {
+        return this;
+    }
+}
