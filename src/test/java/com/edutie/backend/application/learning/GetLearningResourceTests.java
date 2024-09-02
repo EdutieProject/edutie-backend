8bdce65f331ad5e6e6752b51b9b9aac38b004956
sukerberk1 <szymon.l3232@gmail.com>
2024-09-02 21:34:54 +0200

    Add test - PART 2

--- src/test/java/com/edutie/backend/application/learning/GetLearningResourceTests.java
+++ src/test/java/com/edutie/backend/application/learning/GetLearningResourceTests.java
@@ -2,12 +2,17 @@ package com.edutie.backend.application.learning;
 
 import com.edutie.backend.application.learning.learningresource.GetLearningResourceByIdQueryHandler;
 import com.edutie.backend.domain.administration.UserId;
+import com.edutie.backend.domain.common.generationprompt.PromptFragment;
 import com.edutie.backend.domain.personalization.learningresource.LearningResource;
 import com.edutie.backend.domain.personalization.learningresource.entities.Activity;
 import com.edutie.backend.domain.personalization.learningresource.entities.Theory;
 import com.edutie.backend.domain.personalization.learningresource.persistence.LearningResourcePersistence;
+import com.edutie.backend.domain.personalization.learningresourcedefinition.LearningResourceDefinition;
+import com.edutie.backend.domain.personalization.learningresourcedefinition.persistence.LearningResourceDefinitionPersistence;
 import com.edutie.backend.domain.personalization.learningresourcegenerationschema.LearningResourceGenerationSchema;
 import com.edutie.backend.domain.personalization.student.Student;
+import com.edutie.backend.domain.personalization.student.persistence.StudentPersistence;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +23,35 @@ import java.util.Set;
 public class GetLearningResourceTests {
     @Autowired
     private GetLearningResourceByIdQueryHandler getLearningResourceByIdQueryHandler;
+    @Autowired
     private LearningResourcePersistence learningResourcePersistence;
+    @Autowired
+    private LearningResourceDefinitionPersistence learningResourceDefinitionPersistence;
+    @Autowired
+    private StudentPersistence studentPersistence;
     private final UserId userId = new UserId();
+    private final Student student = Student.create(userId);
+
+    @BeforeEach
+    public void testSetup() {
+        studentPersistence.save(student).throwIfFailure();
+    }
 
     @Test
     public void getLearningResourceByIdTest() {
-        LearningResource learningResource = LearningResource.create(null, Activity.create("", Set.of()), Theory.create("",""), Set.of());
+        LearningResourceDefinition learningResourceDefinition = LearningResourceDefinition.create(
+                PromptFragment.of(""),
+                PromptFragment.of(""),
+                Set.of()
+        );
+        learningResourceDefinitionPersistence.save(learningResourceDefinition).throwIfFailure();
+
+        LearningResource learningResource = LearningResource.create(
+                LearningResourceGenerationSchema.create(learningResourceDefinition, student),
+                Activity.create("", Set.of()),
+                Theory.create("",""),
+                Set.of()
+        );
         learningResourcePersistence.save(learningResource).throwIfFailure();
 
         assert learningResourcePersistence.getById(learningResource.getId()).isSuccess();
