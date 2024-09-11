5813678761399963f3b0c44b22bb31365a336eb9
sukerberk1 <szymon.l3232@gmail.com>
2024-09-11 22:23:13 +0200

    Add LRD json property for temporary metadata holding

--- src/main/java/com/edutie/backend/domain/personalization/learningresult/LearningResult.java
+++ src/main/java/com/edutie/backend/domain/personalization/learningresult/LearningResult.java
@@ -1,14 +1,19 @@
 package com.edutie.backend.domain.personalization.learningresult;
 
 import com.edutie.backend.domain.common.base.AuditableEntityBase;
+import com.edutie.backend.domain.personalization.learningresourcedefinition.LearningResourceDefinition;
 import com.edutie.backend.domain.personalization.learningresult.entities.Assessment;
 import com.edutie.backend.domain.personalization.learningresult.identities.LearningResultId;
 import com.edutie.backend.domain.personalization.learningresult.valueobjects.Feedback;
 import com.edutie.backend.domain.personalization.solutionsubmission.SolutionSubmission;
 import com.edutie.backend.domain.personalization.student.Student;
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,39 +29,54 @@ import java.util.Set;
 @NoArgsConstructor
 @Entity
 public class LearningResult extends AuditableEntityBase<LearningResultId> {
-	@OneToMany(targetEntity = Assessment.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	private final Set<Assessment> assessments = new HashSet<>();
-	@Embedded
-	private Feedback feedback = new Feedback();
-	@ManyToOne(targetEntity = SolutionSubmission.class, fetch = FetchType.EAGER)
-	@Setter(AccessLevel.PRIVATE)
-	@JsonIgnore
-	private SolutionSubmission solutionSubmission;
-	@Setter(AccessLevel.PRIVATE)
-	@ManyToOne(targetEntity = Student.class, fetch = FetchType.EAGER)
-	@JoinColumn(name = "student_id")
-	@JsonIgnore
-	private Student student;
+    @OneToMany(targetEntity = Assessment.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private final Set<Assessment> assessments = new HashSet<>();
+    @Embedded
+    private Feedback feedback = new Feedback();
+    @ManyToOne(targetEntity = SolutionSubmission.class, fetch = FetchType.EAGER)
+    @Setter(AccessLevel.PRIVATE)
+    @JsonIgnore
+    private SolutionSubmission solutionSubmission;
+    @Setter(AccessLevel.PRIVATE)
+    @ManyToOne(targetEntity = Student.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "student_id")
+    @JsonIgnore
+    private Student student;
 
-	/**
-	 * Recommended constructor associating learning result with Student and solution submission
-	 *
-	 * @param student            student reference
-	 * @param solutionSubmission solution submission reference
-	 * @return new Learning Result
-	 */
-	public static LearningResult create(Student student, SolutionSubmission solutionSubmission, Feedback feedback) {
-		LearningResult learningResult = new LearningResult();
-		learningResult.setId(new LearningResultId());
-		learningResult.setCreatedBy(student.getOwnerUserId());
-		learningResult.setStudent(student);
-		learningResult.setSolutionSubmission(solutionSubmission);
-		learningResult.setFeedback(feedback);
-		return learningResult;
-	}
+    /**
+     * Recommended constructor associating learning result with Student and solution submission
+     *
+     * @param student            student reference
+     * @param solutionSubmission solution submission reference
+     * @return new Learning Result
+     */
+    public static LearningResult create(Student student, SolutionSubmission solutionSubmission, Feedback feedback) {
+        LearningResult learningResult = new LearningResult();
+        learningResult.setId(new LearningResultId());
+        learningResult.setCreatedBy(student.getOwnerUserId());
+        learningResult.setStudent(student);
+        learningResult.setSolutionSubmission(solutionSubmission);
+        learningResult.setFeedback(feedback);
+        return learningResult;
+    }
 
-	public void addAssessment(Assessment assessment) {
-		assessments.add(assessment);
-	}
+    /**
+     * Adds assessment to this Learning Result
+     *
+     * @param assessment assessment to be added
+     */
+    public void addAssessment(Assessment assessment) {
+        assessments.add(assessment);
+    }
+
+    /**
+     * Getter added mainly for json serialization purposes. Retrieves associated LRD
+     *
+     * @return LRD
+     */
+    @JsonProperty("learningResourceDefinition")
+    public LearningResourceDefinition getLearningResourceDefinition() {
+        return solutionSubmission.getLearningResource().getDefinition();
+    }
 
 }
