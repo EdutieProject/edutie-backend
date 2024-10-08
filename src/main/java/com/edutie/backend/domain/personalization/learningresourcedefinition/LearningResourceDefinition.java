5e2e97eb6041b0f33b2a956389af934562cb2d2b
sukerberk1 <szymon.l3232@gmail.com>
2024-10-08 22:00:35 +0200

    LearningResourceDefinition code refactorings

--- src/main/java/com/edutie/backend/domain/personalization/learningresourcedefinition/LearningResourceDefinition.java
+++ src/main/java/com/edutie/backend/domain/personalization/learningresourcedefinition/LearningResourceDefinition.java
@@ -7,66 +7,107 @@ import com.edutie.backend.domain.education.learningrequirement.LearningRequireme
 import com.edutie.backend.domain.education.learningrequirement.identities.LearningRequirementId;
 import com.edutie.backend.domain.personalization.learningresourcedefinition.identities.LearningResourceDefinitionId;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
 /**
- * Learning Resource Definition is an absolute definition of how Learning Resource should be generated
- * from the knowledge and educational perspective.
+ * Learning Resource Definition is a definition of how Learning Resource should be generated
+ * from the knowledge and educational perspective. LRs can be generated using only prompt fragments, but
+ * the learning requirements are imperative regarding the LR creation.
  */
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
 public class LearningResourceDefinition extends EducatorCreatedAuditableEntity<LearningResourceDefinitionId> {
-	@ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-	private final Set<LearningRequirement> learningRequirements = new HashSet<>();
-	@Embedded
-	@AttributeOverride(name = "text", column = @Column(name = "theory_description", columnDefinition = "TEXT"))
-	private PromptFragment theoryDescription;
-	@Embedded
-	@AttributeOverride(name = "text", column = @Column(name = "graph_description", columnDefinition = "TEXT"))
-	private PromptFragment graphDescription;
-	@Embedded
-	@AttributeOverride(name = "text", column = @Column(name = "exercise_description", columnDefinition = "TEXT"))
-	private PromptFragment exerciseDescription;
-	@Embedded
-	@AttributeOverride(name = "text", column = @Column(name = "hints_description", columnDefinition = "TEXT"))
-	private PromptFragment hintsAdditionalDescription;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private final Set<LearningRequirement> learningRequirements = new HashSet<>();
+    @Embedded
+    @AttributeOverride(name = "text", column = @Column(name = "theory_description", columnDefinition = "TEXT"))
+    private PromptFragment theoryDescription;
+    @Embedded
+    @AttributeOverride(name = "text", column = @Column(name = "graph_description", columnDefinition = "TEXT"))
+    private PromptFragment graphDescription;
+    @Embedded
+    @AttributeOverride(name = "text", column = @Column(name = "exercise_description", columnDefinition = "TEXT"))
+    private PromptFragment exerciseDescription;
+    @Embedded
+    @AttributeOverride(name = "text", column = @Column(name = "hints_description", columnDefinition = "TEXT"))
+    private PromptFragment hintsAdditionalDescription;
 
-	public static LearningResourceDefinition create(Educator educator, PromptFragment theoryDescription, PromptFragment exerciseDescription, PromptFragment theorySummaryAdditionalDescription, PromptFragment hintsAdditionalDescription) {
-		LearningResourceDefinition lrd = new LearningResourceDefinition();
-		lrd.setId(new LearningResourceDefinitionId());
-		lrd.setAuthorEducator(educator);
-		lrd.setCreatedBy(educator.getOwnerUserId());
-		lrd.setTheoryDescription(theoryDescription);
-		lrd.setExerciseDescription(exerciseDescription);
-		lrd.setGraphDescription(theorySummaryAdditionalDescription);
-		lrd.setHintsAdditionalDescription(hintsAdditionalDescription);
-		return lrd;
-	}
+    /**
+     * Creates a Learning Resource Definition with full specification provided.
+     *
+     * @param educator                   educator profile
+     * @param theoryDescription          theory description prompt fragment
+     * @param exerciseDescription        exercise description prompt fragment
+     * @param graphDescription           graph description prompt fragment
+     * @param hintsAdditionalDescription hints additional description prompt fragment
+     * @param learningRequirements       learning requirements set
+     * @return new Learning Resource Definition
+     */
+    public static LearningResourceDefinition create(
+            Educator educator,
+            PromptFragment theoryDescription,
+            PromptFragment exerciseDescription,
+            PromptFragment graphDescription,
+            PromptFragment hintsAdditionalDescription,
+            Set<LearningRequirement> learningRequirements
+    ) {
+        LearningResourceDefinition lrd = new LearningResourceDefinition();
+        lrd.setId(new LearningResourceDefinitionId());
+        lrd.setAuthorEducator(educator);
+        lrd.setCreatedBy(educator.getOwnerUserId());
+        lrd.setTheoryDescription(theoryDescription);
+        lrd.setExerciseDescription(exerciseDescription);
+        lrd.setGraphDescription(graphDescription);
+        lrd.setHintsAdditionalDescription(hintsAdditionalDescription);
+        lrd.learningRequirements.addAll(learningRequirements);
+        return lrd;
+    }
 
-	public static LearningResourceDefinition create(PromptFragment theoryDescription, PromptFragment exerciseDescription, Set<LearningRequirement> learningRequirements) {
-		LearningResourceDefinition learningResourceDefinition = new LearningResourceDefinition();
-		learningResourceDefinition.setId(new LearningResourceDefinitionId());
-		learningResourceDefinition.setTheoryDescription(theoryDescription);
-		learningResourceDefinition.setExerciseDescription(exerciseDescription);
-		learningResourceDefinition.learningRequirements.addAll(learningRequirements);
-		return learningResourceDefinition;
-	}
+    /**
+     * Creates an LRD with only mandatory descriptors but without learning requirements.
+     *
+     * @param educator            educator profile
+     * @param theoryDescription   theory description prompt fragment
+     * @param exerciseDescription exercise description prompt fragment
+     * @return new Learning Resource Definition
+     */
+    public static LearningResourceDefinition create(Educator educator, PromptFragment theoryDescription, PromptFragment exerciseDescription) {
+        return create(educator, theoryDescription, exerciseDescription, null, null, Set.of());
+    }
 
-	public Optional<LearningRequirement> getLearningRequirementOfId(LearningRequirementId learningRequirementId) {
-		return learningRequirements.stream().filter(o -> o.getId().equals(learningRequirementId)).findFirst();
-	}
+    /**
+     * Retrieves learning requirement of the given id if it is associated with this LRD.
+     *
+     * @param learningRequirementId learning requirement Id
+     * @return Optional Learning Requirement
+     */
+    public Optional<LearningRequirement> getLearningRequirementOfId(LearningRequirementId learningRequirementId) {
+        return learningRequirements.stream().filter(o -> o.getId().equals(learningRequirementId)).findFirst();
+    }
 
-	public void addLearningRequirement(LearningRequirement learningRequirement) {
-		learningRequirements.add(learningRequirement);
-	}
+    /**
+     * Adds learning requirement to this LRD
+     *
+     * @param learningRequirement learning requirement to add
+     */
+    public void addLearningRequirement(LearningRequirement learningRequirement) {
+        learningRequirements.add(learningRequirement);
+    }
 
-	public void removeLearningRequirement(LearningRequirementId learningRequirementId) {
-		learningRequirements.removeIf(o -> o.getId().equals(learningRequirementId));
-	}
+    /**
+     * Removes learning requirement given its id
+     *
+     * @param learningRequirementId learning requirement Id
+     */
+    public void removeLearningRequirement(LearningRequirementId learningRequirementId) {
+        learningRequirements.removeIf(o -> o.getId().equals(learningRequirementId));
+    }
 }
