318b79ab422ca35354a22fadc6f07716238367a5
sukerberk1 <szymon.l3232@gmail.com>
2024-10-08 23:04:11 +0200

    Add random fact LR flow port

---
+++ src/main/java/com/edutie/backend/application/learning/learningresource/commands/CreateRandomFactDynamicLearningResourceCommand.java
@@ -0,0 +1,26 @@
+package com.edutie.backend.application.learning.learningresource.commands;
+
+import com.edutie.backend.application.common.actions.StudentAction;
+import com.edutie.backend.domain.personalization.learningresourcedefinition.identities.LearningResourceDefinitionId;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(fluent = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public final class CreateRandomFactDynamicLearningResourceCommand extends StudentAction<CreateRandomFactDynamicLearningResourceCommand> {
+	@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+	private @NonNull String randomFact;
+
+	@Override
+	protected CreateRandomFactDynamicLearningResourceCommand getThis() {
+		return this;
+	}
+}
