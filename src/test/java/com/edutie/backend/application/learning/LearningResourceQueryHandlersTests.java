b88caea0a0e1ec4c3430db4930fdadb1bfd57bd9
sukerberk1 <szymon.l3232@gmail.com>
2024-10-28 21:03:30 +0100

    Add application learning handlers tests

--- src/test/java/com/edutie/backend/application/learning/LearningResourceQueryHandlersTests.java
+++ src/test/java/com/edutie/backend/application/learning/LearningResourceQueryHandlersTests.java
@@ -1,13 +1,10 @@
 package com.edutie.backend.application.learning;
 
 import com.edutie.backend.application.learning.learningresource.GetLearningResourceByIdQueryHandler;
+import com.edutie.backend.application.learning.learningresource.GetLearningResourcesByDefinitionIdQueryHandler;
 import com.edutie.backend.application.learning.learningresource.queries.GetLearningResourceByIdQuery;
-import com.edutie.backend.domain.administration.UserId;
-import com.edutie.backend.domain.administration.administrator.Administrator;
-import com.edutie.backend.domain.administration.administrator.persistence.AdministratorPersistence;
+import com.edutie.backend.application.learning.learningresource.queries.GetLearningResourcesByDefinitionIdQuery;
 import com.edutie.backend.domain.common.generationprompt.PromptFragment;
-import com.edutie.backend.domain.education.educator.Educator;
-import com.edutie.backend.domain.education.educator.persistence.EducatorPersistence;
 import com.edutie.backend.domain.personalization.learningresource.LearningResource;
 import com.edutie.backend.domain.personalization.learningresource.entities.Activity;
 import com.edutie.backend.domain.personalization.learningresource.entities.Theory;
@@ -17,15 +14,16 @@ import com.edutie.backend.domain.personalization.learningresourcedefinition.enti
 import com.edutie.backend.domain.personalization.learningresourcedefinition.entities.TheoryDetails;
 import com.edutie.backend.domain.personalization.learningresourcedefinition.persistence.LearningResourceDefinitionPersistence;
 import com.edutie.backend.domain.personalization.learningresult.persistence.LearningResultPersistence;
-import com.edutie.backend.domain.personalization.student.Student;
-import com.edutie.backend.domain.personalization.student.persistence.StudentPersistence;
 import com.edutie.backend.domainservice.personalization.learningresource.schema.LearningResourceGenerationSchema;
+import com.edutie.backend.mocks.MockUser;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import validation.WrapperResult;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,57 +31,74 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
-public class GetLearningResourceTests {
-    private final UserId userId = new UserId();
-    private final Student student = Student.create(userId);
-    private final Administrator administrator = Administrator.create(userId);
-    private final Educator educator = Educator.create(userId, administrator);
+public class LearningResourceQueryHandlersTests {
     @Autowired
-    private AdministratorPersistence administratorPersistence;
-    @Autowired
-    private EducatorPersistence educatorPersistence;
-    @Autowired
-    private GetLearningResourceByIdQueryHandler getLearningResourceByIdQueryHandler;
+    MockUser mockUser;
+    // Persistence
     @Autowired
     private LearningResourcePersistence learningResourcePersistence;
     @Autowired
     private LearningResultPersistence learningResultPersistence;
     @Autowired
     private LearningResourceDefinitionPersistence learningResourceDefinitionPersistence;
+    // Handlers
     @Autowired
-    private StudentPersistence studentPersistence;
+    private GetLearningResourceByIdQueryHandler getLearningResourceByIdQueryHandler;
+    @Autowired
+    private GetLearningResourcesByDefinitionIdQueryHandler getLearningResourcesByDefinitionIdQueryHandler;
 
     @BeforeEach
     public void testSetup() {
-        administratorPersistence.save(administrator).throwIfFailure();
-        studentPersistence.save(student).throwIfFailure();
-        educatorPersistence.save(educator).throwIfFailure();
+        mockUser.saveToPersistence();
     }
 
-    @Test
-    public void getLearningResourceByIdTest() {
+    private LearningResourceDefinition createAndSaveLearningResourceDefinition() {
         LearningResourceDefinition learningResourceDefinition = LearningResourceDefinition.create(
-                educator,
+                mockUser.getEducatorProfile(),
                 TheoryDetails.create(PromptFragment.empty(), PromptFragment.empty()),
                 ActivityDetails.create(PromptFragment.empty(), PromptFragment.empty()),
                 Set.of()
         );
         learningResourceDefinitionPersistence.save(learningResourceDefinition).throwIfFailure();
+        return learningResourceDefinition;
+    }
 
+    private LearningResource createAndSaveLearningResource(LearningResourceDefinition learningResourceDefinition) {
         LearningResource learningResource = LearningResource.create(
-                LearningResourceGenerationSchema.create(student, learningResultPersistence, Set.of(), learningResourceDefinition),
+                LearningResourceGenerationSchema.create(mockUser.getStudentProfile(), learningResultPersistence, Set.of(), learningResourceDefinition),
                 Activity.create("", Set.of()),
                 Theory.create("", "")
         );
         learningResourcePersistence.save(learningResource).throwIfFailure();
+        return learningResource;
+    }
+
+    @Test
+    public void getLearningResourceByIdTest() {
+        LearningResourceDefinition learningResourceDefinition = createAndSaveLearningResourceDefinition();
+        LearningResource learningResource = createAndSaveLearningResource(learningResourceDefinition);
 
         GetLearningResourceByIdQuery query = new GetLearningResourceByIdQuery()
-                .studentUserId(userId)
+                .studentUserId(mockUser.getUserId())
                 .learningResourceId(learningResource.getId());
 
         WrapperResult<LearningResource> queryResult = getLearningResourceByIdQueryHandler.handle(query);
 
         assertTrue(queryResult.isSuccess());
         assertEquals(queryResult.getValue(), learningResource);
+    }
+
+    @Test
+    public void getLearningResourcesByDefinitionIdTest() {
+        LearningResourceDefinition learningResourceDefinition = createAndSaveLearningResourceDefinition();
+        LearningResource learningResource = createAndSaveLearningResource(learningResourceDefinition);
+
+        GetLearningResourcesByDefinitionIdQuery query = new GetLearningResourcesByDefinitionIdQuery()
+                .learningResourceDefinitionId(learningResourceDefinition.getId())
+                .studentUserId(mockUser.getUserId());
+        WrapperResult<List<LearningResource>> learningResourcesWrapper = getLearningResourcesByDefinitionIdQueryHandler.handle(query);
+
+        Assertions.assertTrue(learningResourcesWrapper.isSuccess());
+        Assertions.assertTrue(learningResourcesWrapper.getValue().contains(learningResource));
     }
 }
