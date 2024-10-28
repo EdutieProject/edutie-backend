671722888113292a9efcd39aa4825588b732ccb3
sukerberk1 <szymon.l3232@gmail.com>
2024-10-28 14:37:14 +0100

    Add LearningResourcePersonalizationServiceTests

---
+++ src/test/java/com/edutie/backend/domainservice/LearningResourcePersonalizationServiceTests.java
@@ -0,0 +1,36 @@
+package com.edutie.backend.domainservice;
+
+import com.edutie.backend.domain.personalization.learningresult.persistence.LearningResultPersistence;
+import com.edutie.backend.domainservice.personalization.learningresource.LearningResourcePersonalizationService;
+import com.edutie.backend.domainservice.personalization.learningresource.implementation.LearningResourcePersonalizationServiceImplementation;
+import com.edutie.backend.mocks.ExternalServiceMocks;
+import com.edutie.backend.mocks.MockUser;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LearningResourcePersonalizationServiceTests {
+    @Autowired
+    private MockUser mockUser;
+
+    @Autowired
+    private LearningResultPersistence learningResultPersistence;
+
+    private LearningResourcePersonalizationService learningResourcePersonalizationService = new LearningResourcePersonalizationServiceImplementation(
+            ExternalServiceMocks.knowledgeMapServiceMock(),
+            learningResultPersistence,
+            ExternalServiceMocks.largeLanguageModelServiceMock()
+    );
+
+    @BeforeEach
+    public void testSetup() {
+        mockUser.saveToPersistence();
+    }
+
+    @Test
+    public void personalizationServiceTest() {
+        //TODO after personalization v 2 implementation
+    }
+}
