9b30e9b5dc217b47a73ffb843f39fd1646f51bf0
sukerberk1 <szymon.l3232@gmail.com>
2024-11-02 11:01:18 +0100

    Adjust selection algorithm

--- src/main/java/com/edutie/backend/domainservice/personalization/learningrequirement/implementation/DynamicLearningRequirementSelectionServiceImplementation.java
+++ src/main/java/com/edutie/backend/domainservice/personalization/learningrequirement/implementation/DynamicLearningRequirementSelectionServiceImplementation.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import validation.WrapperResult;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,9 +23,19 @@ public class DynamicLearningRequirementSelectionServiceImplementation implements
 
     @Override
     public WrapperResult<Set<LearningRequirement>> selectRequirementsForStudent(Student student) {
-        Set<LearningRequirement> learningRequirements = student.getLatestAssessmentsByMaxGrade(learningResultPersistence, new Grade(3))
-                .stream().map(o -> learningRequirementPersistence.getById(o.getLearningRequirementId()).getValue()).collect(Collectors.toSet());
+        List<LearningRequirement> latestWeakPerformanceRequirements = student.getLatestAssessmentsByMaxGrade(learningResultPersistence, new Grade(3))
+                .stream().map(o -> learningRequirementPersistence.getById(o.getLearningRequirementId()).getValue()).toList();
+        // Now let us choose random L-Reqs. In the future, we will replace them with correlated ones.
+        List<LearningRequirement> randomLearningRequirements = learningRequirementPersistence.getAny(2).getValue();
 
-        return WrapperResult.successWrapper(!learningRequirements.isEmpty() ? learningRequirements : new HashSet<>(learningRequirementPersistence.getAny(2).getValue()));
+        if (latestWeakPerformanceRequirements.isEmpty())
+            return WrapperResult.successWrapper(new HashSet<>(randomLearningRequirements));
+        // Randomize - either return random req, random + one of the latest, or just one of the latest
+        int randomNumber = (int) Math.ceil(Math.random() * 3);
+        return WrapperResult.successWrapper(
+                randomNumber == 1 ? Set.of(latestWeakPerformanceRequirements.getFirst()) :
+                        randomNumber == 2 ? Set.of(randomLearningRequirements.getFirst()) :
+                                Set.of(randomLearningRequirements.getFirst(), latestWeakPerformanceRequirements.getFirst())
+        );
     }
 }
