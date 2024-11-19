54f69ad8540fc98c3a2207db43d5bc4811de16d4
sukerberk1 <szymon.l3232@gmail.com>
2024-11-19 22:09:57 +0100

    minor fix

--- src/main/java/com/edutie/backend/infrastructure/external/knowledgemap/messages/MostCorrelatedSubjectResponse.java
+++ src/main/java/com/edutie/backend/infrastructure/external/knowledgemap/messages/MostCorrelatedSubjectResponse.java
@@ -6,15 +6,13 @@ import com.edutie.backend.infrastructure.external.common.dto.ExternalInfrastruct
 
 public record MostCorrelatedSubjectResponse(
         String id,
-        String title,
-        String description
+        String title
 ) implements ExternalInfrastructureDto<KnowledgeSubject, MostCorrelatedSubjectRequest> {
     @Override
     public KnowledgeSubject intoDomainEntity(MostCorrelatedSubjectRequest mostCorrelatedSubjectRequest) {
         return KnowledgeSubject.create(
                 new KnowledgeSubjectId(id),
-                title,
-                description
+                title
         );
     }
 }
