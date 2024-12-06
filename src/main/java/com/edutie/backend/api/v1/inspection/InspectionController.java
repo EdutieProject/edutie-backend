a2f8485e3e3a25c1d5db204f24a9711fbf8f611b
sukerberk1 <szymon.l3232@gmail.com>
2024-12-06 21:26:24 +0100

    Inspection section fix

--- src/main/java/com/edutie/backend/api/v1/inspection/InspectionController.java
+++ src/main/java/com/edutie/backend/api/v1/inspection/InspectionController.java
@@ -6,6 +6,7 @@ import com.edutie.backend.infrastructure.authorization.administrator.Administrat
 import com.edutie.backend.infrastructure.authorization.educator.EducatorAuthorization;
 import com.edutie.backend.infrastructure.authorization.student.StudentAuthorization;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.util.MultiValueMapAdapter;
 import validation.Result;
 import validation.WrapperResult;
 import org.springframework.http.*;
@@ -15,6 +16,8 @@ import org.springframework.security.oauth2.server.resource.authentication.*;
 import org.springframework.web.bind.annotation.*;
 import lombok.*;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -22,7 +25,7 @@ import java.util.stream.Collectors;
 @RequestMapping("api/v1/inspection")
 @RequiredArgsConstructor
 @Tag(name = "Inspection Controller", description = "Controller made for inspection purposes, such as healthcheck or authentication validity check.")
-public class TestController {
+public class InspectionController {
 	private final AdministratorAuthorization administratorAuthorization;
 	private final EducatorAuthorization educatorAuthorization;
 	private final StudentAuthorization studentAuthorization;
@@ -33,16 +36,17 @@ public class TestController {
 	}
 
 	@GetMapping("/test-authentication")
-	public String authenticated(Authentication authentication) {
-		if (authentication == null || !authentication.isAuthenticated()) {
-			return Result.failure(AuthenticationError.invalidAuthentication()).toString();
-		}
+	public ResponseEntity<String> authenticated(Authentication authentication) {
 		if (authentication instanceof JwtAuthenticationToken jwtAuthenticationToken) {
 			UserId userId = new UserId(UUID.fromString(jwtAuthenticationToken.getTokenAttributes().get(JwtClaimNames.SUB).toString()));
 			String name = (String) jwtAuthenticationToken.getTokenAttributes().get("name");
-			return "AUTH SUCCESS! Look, its you: " + userId + "\nYour name here: " + name;
+			return ResponseEntity.ok("AUTH SUCCESS! Look, its you: " + userId + "\nYour name here: " + name);
 		}
-		return Result.failure(AuthenticationError.noJwtAuthentication()).toString();
+		return new ResponseEntity<>(
+				Result.failure(AuthenticationError.invalidAuthentication()).toString(),
+				new MultiValueMapAdapter<>(new HashMap<>()),
+				HttpStatus.UNAUTHORIZED
+		);
 	}
 
 	@GetMapping("/test-roles")
