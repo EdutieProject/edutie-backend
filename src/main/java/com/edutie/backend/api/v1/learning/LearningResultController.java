e1e9dae6b4bc547d4cb5e4951dfac89163438cb1
sukerberk1 <szymon.l3232@gmail.com>
2024-12-27 19:50:57 +0100

    Solution submission view endpoint

--- src/main/java/com/edutie/backend/api/v1/learning/LearningResultController.java
+++ src/main/java/com/edutie/backend/api/v1/learning/LearningResultController.java
@@ -4,11 +4,14 @@ import com.edutie.backend.api.common.ApiResult;
 import com.edutie.backend.api.common.GenericRequestHandler;
 import com.edutie.backend.application.learning.learningresult.AssessSolutionCommandHandler;
 import com.edutie.backend.application.learning.learningresult.GetLearningResultByIdQueryHandler;
+import com.edutie.backend.application.learning.learningresult.GetSolutionSubmissionByIdQueryHandler;
 import com.edutie.backend.application.learning.learningresult.commands.AssessSolutionCommand;
 import com.edutie.backend.application.learning.learningresult.queries.GetLearningResultByIdQuery;
+import com.edutie.backend.application.learning.learningresult.queries.GetSolutionSubmissionByIdQuery;
 import com.edutie.backend.domain.personalization.learningresult.LearningResult;
-import com.edutie.backend.domain.personalization.learningresource.LearningResource;
 import com.edutie.backend.domain.personalization.learningresult.identities.LearningResultId;
+import com.edutie.backend.domain.personalization.solutionsubmission.SolutionSubmission;
+import com.edutie.backend.domain.personalization.solutionsubmission.identities.SolutionSubmissionId;
 import com.edutie.backend.infrastructure.authorization.student.StudentAuthorization;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,22 +25,34 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Tag(name = "Learning Result Controller", description = "Provides operations regarding learning results in the learning context")
 public class LearningResultController {
-	private final StudentAuthorization studentAuthorization;
-	private final GetLearningResultByIdQueryHandler getLearningResultByIdQueryHandler;
-	private final AssessSolutionCommandHandler assessSolutionCommandHandler;
+    private final StudentAuthorization studentAuthorization;
+    private final GetLearningResultByIdQueryHandler getLearningResultByIdQueryHandler;
+    private final AssessSolutionCommandHandler assessSolutionCommandHandler;
+    private final GetSolutionSubmissionByIdQueryHandler getSolutionSubmissionByIdQueryHandler;
 
-	@GetMapping("/{learningResultId}")
-	@Operation(description = "Retrieves a learning result by its identifier")
-	public ResponseEntity<ApiResult<LearningResource>> getLearningResultById(Authentication authentication, @PathVariable LearningResultId learningResultId) {
-		return new GenericRequestHandler<LearningResource>()
-				.authenticate(authentication)
-				.authorize(studentAuthorization)
-				.handle((userId) -> getLearningResultByIdQueryHandler.handle(
-						new GetLearningResultByIdQuery().learningResultId(learningResultId).studentUserId(userId))
-				);
-	}
+    @GetMapping("/{learningResultId}")
+    @Operation(description = "Retrieves a learning result by its identifier")
+    public ResponseEntity<ApiResult<LearningResult>> getLearningResultById(Authentication authentication, @PathVariable LearningResultId learningResultId) {
+        return new GenericRequestHandler<LearningResult>()
+                .authenticate(authentication)
+                .authorize(studentAuthorization)
+                .handle((userId) -> getLearningResultByIdQueryHandler.handle(
+                        new GetLearningResultByIdQuery().learningResultId(learningResultId).studentUserId(userId))
+                );
+    }
 
-	@PostMapping("/create-from-solution")
+    @GetMapping("/{solutionSubmissionId}")
+    @Operation(description = "Retrieves a learning result by its identifier")
+    public ResponseEntity<ApiResult<SolutionSubmission>> getLearningResultById(Authentication authentication, @PathVariable SolutionSubmissionId solutionSubmissionId) {
+        return new GenericRequestHandler<SolutionSubmission>()
+                .authenticate(authentication)
+                .authorize(studentAuthorization)
+                .handle((userId) -> getSolutionSubmissionByIdQueryHandler.handle(
+                        new GetSolutionSubmissionByIdQuery().studentUserId(userId).solutionSubmissionId(solutionSubmissionId)
+                ));
+    }
+
+    @PostMapping("/create-from-solution")
     @Operation(description = "Assesses the solution provided in the command and returns a Learning Result.")
     public ResponseEntity<ApiResult<LearningResult>> assessSolution(Authentication authentication, @RequestBody AssessSolutionCommand command) {
         return new GenericRequestHandler<LearningResult>()
