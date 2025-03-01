4c8d8fa5ba10950e97e87e8c7ff2e61e7fe79b0a
sukerberk1 <szymon.l3232@gmail.com>
2025-03-02 00:07:23 +0100

    ScaffoldLearningSubjectRequirementsCommandHandler - port

---
+++ src/main/java/com/edutie/application/management/learningsubject/command/ScaffoldLearningSubjectRequirementsCommand.java
@@ -0,0 +1,28 @@
+package com.edutie.application.management.learningsubject.command;
+
+import com.edutie.application.common.actions.EducatorAction;
+import com.edutie.domain.core.education.learningsubject.identities.LearningSubjectId;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(fluent = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public final class ScaffoldLearningSubjectRequirementsCommand extends EducatorAction<ScaffoldLearningSubjectRequirementsCommand> {
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private @NonNull LearningSubjectId learningSubjectId;
+    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String additionalContext;
+
+    @Override
+    protected ScaffoldLearningSubjectRequirementsCommand getThis() {
+        return this;
+    }
+}
