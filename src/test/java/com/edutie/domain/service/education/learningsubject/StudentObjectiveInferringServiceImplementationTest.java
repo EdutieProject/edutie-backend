27f05713841b6dd06d46ffb32bd23194879d3c05
sukerberk1 <szymon.l3232@gmail.com>
2025-03-04 20:11:04 +0100

    StudentObjectiveInferringServiceImplementationTest.java

--- src/test/java/com/edutie/domain/service/education/learningsubject/StudentObjectiveInferringServiceImplementationTest.java
+++ src/test/java/com/edutie/domain/service/education/learningsubject/StudentObjectiveInferringServiceImplementationTest.java
@@ -1,7 +1,13 @@
 package com.edutie.domain.service.education.learningsubject;
 
+import com.edutie.domain.core.common.generationprompt.PromptFragment;
+import com.edutie.domain.core.education.learningsubject.entities.KnowledgeOrigin;
+import com.edutie.domain.core.education.learningsubject.service.StudentObjectiveInferringService;
+import com.edutie.infrastructure.knowledgemap.learningsubject.StudentObjectiveGenerationService;
+import com.edutie.infrastructure.knowledgemap.learningsubject.schema.StudentObjectiveGenerationSchema;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import validation.WrapperResult;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +16,14 @@ class StudentObjectiveInferringServiceImplementationTest {
 
     @Test
     void getStudentObjective() {
-        //TODO
+        StudentObjectiveInferringService studentObjectiveInferringService = new StudentObjectiveInferringServiceImplementation(
+                generationSchema -> WrapperResult.successWrapper(generationSchema.objectiveTitle() + " Hello World!")
+        );
+
+        WrapperResult<PromptFragment> studentObjectiveResult = studentObjectiveInferringService.getStudentObjective("Hey there!", new KnowledgeOrigin());
+
+        assertTrue(studentObjectiveResult.isSuccess());
+        PromptFragment promptFragment = studentObjectiveResult.getValue();
+        assertTrue(promptFragment.equals(PromptFragment.of("Hey there! Hello World!")));
     }
 }
---
+++ src/test/java/com/edutie/domain/service/learning/student/StudentLearningHistoryServiceImplementationTest.java
@@ -0,0 +1,10 @@
+package com.edutie.domain.service.learning.student;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentLearningHistoryServiceImplementationTest {
+    // nothing to test as of now
+}
