98941f537da70f1876d6a0f1ad7470fd30733433
sukerberk1 <szymon.l3232@gmail.com>
2025-03-05 21:42:43 +0100

    Add test & api endpoint

--- src/main/java/com/edutie/api/v1/management/LearningSubjectController.java
+++ src/main/java/com/edutie/api/v1/management/LearningSubjectController.java
@@ -2,27 +2,21 @@ package com.edutie.api.v1.management;
 
 import com.edutie.api.common.ApiResult;
 import com.edutie.api.common.GenericRequestHandler;
-import com.edutie.application.learning.learningexperience.command.CreateSimilarLearningExperienceCommand;
-import com.edutie.application.management.learningsubject.AddLearningSubjectRequirementCommandHandler;
-import com.edutie.application.management.learningsubject.CreateBlankLearningSubjectCommandHandler;
-import com.edutie.application.management.learningsubject.RemoveLearningSubjectRequirementCommandHandler;
-import com.edutie.application.management.learningsubject.SetLearningSubjectKnowledgeSubjectCommandHandler;
+import com.edutie.application.management.learningsubject.*;
 import com.edutie.application.management.learningsubject.command.AddLearningSubjectRequirementCommand;
 import com.edutie.application.management.learningsubject.command.CreateBlankLearningSubjectCommand;
 import com.edutie.application.management.learningsubject.command.RemoveLearningSubjectRequirementCommand;
 import com.edutie.application.management.learningsubject.command.SetLearningSubjectKnowledgeSubjectCommand;
+import com.edutie.application.management.learningsubject.query.GetLearningSubjectByIdQuery;
 import com.edutie.domain.core.education.learningsubject.LearningSubject;
-import com.edutie.domain.core.learning.learningexperience.LearningExperience;
+import com.edutie.domain.core.education.learningsubject.identities.LearningSubjectId;
 import com.edutie.infrastructure.authorization.educator.EducatorAuthorization;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/learning-subject")
@@ -30,17 +24,34 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Learning Subject Controller", description = "Provides operations regarding learning subjects in the management context")
 public class LearningSubjectController {
     private final EducatorAuthorization educatorAuthorization;
+    private final GetLearningSubjectByIdQueryHandler getLearningSubjectByIdQueryHandler;
     private final CreateBlankLearningSubjectCommandHandler createBlankLearningSubjectCommandHandler;
     private final AddLearningSubjectRequirementCommandHandler addLearningSubjectRequirementCommandHandler;
     private final RemoveLearningSubjectRequirementCommandHandler removeLearningSubjectRequirementCommandHandler;
     private final SetLearningSubjectKnowledgeSubjectCommandHandler setLearningSubjectKnowledgeSubjectCommandHandler;
+
+    @GetMapping("/{learningSubjectId}")
+    @Operation(description = """
+            Creates a blank learning subject assigning its name.
+            """)
+    public ResponseEntity<ApiResult<LearningSubject>> createBlankLearningSubject(Authentication authentication,
+                                                                                 @PathVariable LearningSubjectId learningSubjectId) {
+        return new GenericRequestHandler<LearningSubject>()
+                .authenticate(authentication)
+                .authorize(educatorAuthorization)
+                .handle((userId) -> getLearningSubjectByIdQueryHandler.handle(
+                        new GetLearningSubjectByIdQuery()
+                                .educatorUserId(userId)
+                                .learningSubjectId(learningSubjectId)
+                ));
+    }
 
     @PostMapping("/create")
     @Operation(description = """
             Creates a blank learning subject assigning its name.
             """)
     public ResponseEntity<ApiResult<LearningSubject>> createBlankLearningSubject(Authentication authentication,
-                                                                                    @RequestBody CreateBlankLearningSubjectCommand command) {
+                                                                                 @RequestBody CreateBlankLearningSubjectCommand command) {
         return new GenericRequestHandler<LearningSubject>()
                 .authenticate(authentication)
                 .authorize(educatorAuthorization)
@@ -54,7 +65,7 @@ public class LearningSubjectController {
             Adds a requirement for the learning subject
             """)
     public ResponseEntity<ApiResult<LearningSubject>> addLearningSubjectRequirement(Authentication authentication,
-                                                                                 @RequestBody AddLearningSubjectRequirementCommand command) {
+                                                                                    @RequestBody AddLearningSubjectRequirementCommand command) {
         return new GenericRequestHandler<LearningSubject>()
                 .authenticate(authentication)
                 .authorize(educatorAuthorization)
@@ -68,7 +79,7 @@ public class LearningSubjectController {
             Removes a requirement from the learning subject
             """)
     public ResponseEntity<ApiResult<LearningSubject>> removeLearningSubjectRequirement(Authentication authentication,
-                                                                                    @RequestBody RemoveLearningSubjectRequirementCommand command) {
+                                                                                       @RequestBody RemoveLearningSubjectRequirementCommand command) {
         return new GenericRequestHandler<LearningSubject>()
                 .authenticate(authentication)
                 .authorize(educatorAuthorization)
@@ -82,7 +93,7 @@ public class LearningSubjectController {
             Sets learning subject's knowledge subject id
             """)
     public ResponseEntity<ApiResult<LearningSubject>> setLearningSubjectKnowledgeSubjectId(Authentication authentication,
-                                                                                       @RequestBody SetLearningSubjectKnowledgeSubjectCommand command) {
+                                                                                           @RequestBody SetLearningSubjectKnowledgeSubjectCommand command) {
         return new GenericRequestHandler<LearningSubject>()
                 .authenticate(authentication)
                 .authorize(educatorAuthorization)
---
+++ src/test/java/com/edutie/application/management/learningsubject/implementation/GetLearningSubjectByIdQueryHandlerImplementationTest.java
@@ -0,0 +1,39 @@
+package com.edutie.application.management.learningsubject.implementation;
+
+import com.edutie.TestUtils;
+import com.edutie.application.management.learningsubject.GetLearningSubjectByIdQueryHandler;
+import com.edutie.application.management.learningsubject.query.GetLearningSubjectByIdQuery;
+import com.edutie.domain.core.administration.UserId;
+import com.edutie.domain.core.education.learningsubject.LearningSubject;
+import com.edutie.domain.core.education.learningsubject.identities.LearningSubjectId;
+import com.edutie.infrastructure.persistence.implementation.education.repositories.LearningSubjectRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import validation.WrapperResult;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GetLearningSubjectByIdQueryHandlerImplementationTest {
+    @Autowired
+    private GetLearningSubjectByIdQueryHandler getLearningSubjectByIdQueryHandler;
+    @Autowired
+    private LearningSubjectRepository learningSubjectRepository;
+
+    @Test
+    public void handle() throws Throwable {
+        LearningSubject learningSubject = new LearningSubject();
+        TestUtils.setPrivateField(learningSubject, "id", new LearningSubjectId());
+        learningSubjectRepository.save(learningSubject);
+
+        GetLearningSubjectByIdQuery query = new GetLearningSubjectByIdQuery()
+                .educatorUserId(new UserId()) // user id doesnt matter
+                .learningSubjectId(learningSubject.getId());
+
+        WrapperResult<LearningSubject> result = getLearningSubjectByIdQueryHandler.handle(query);
+
+        assertTrue(result.isSuccess());
+        assertEquals(learningSubject, result.getValue());
+    }
+}
