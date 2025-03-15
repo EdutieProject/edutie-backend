6375aa4772ce34d794f7f069e005ff921d02d066
sukerberk1 <szymon.l3232@gmail.com>
2025-03-15 22:34:50 +0100

    Remove AbsoluteDefinition.java

--- src/main/java/com/edutie/domain/core/personalization/common/AbsoluteDefinition.java
+++
@@ -1,9 +0,0 @@
-package com.edutie.domain.core.personalization.common;
-
-/**
- * The supertype for the entities that are absolute data sources for personalization purposes.
- * Visualisation:
- * Absolute Definition + Student ----[personalization-service]----> Personalized Resource
- */
-public interface AbsoluteDefinition {
-}
