c33d6a7ca5bda06144a39592f820556b96681896
sukerberk1 <szymon.l3232@gmail.com>
2025-03-18 12:46:01 +0100

    LearningExperienceGenerationSchema fix

--- src/main/java/com/edutie/infrastructure/llm/learningexperience/schema/LearningExperienceGenerationSchema.java
+++ src/main/java/com/edutie/infrastructure/llm/learningexperience/schema/LearningExperienceGenerationSchema.java
@@ -3,15 +3,22 @@ package com.edutie.infrastructure.llm.learningexperience.schema;
 import com.edutie.domain.core.common.generationprompt.PromptFragment;
 import com.edutie.domain.core.education.elementalrequirement.ElementalRequirement;
 import com.edutie.domain.core.learning.learningexperience.entities.activity.base.Activity;
-import com.edutie.domain.core.personalization.learningexperience.contentadjustment.base.ContentAdjustmentStrategy;
+import com.edutie.domain.core.personalization.common.PersonalizationRule;
 import com.edutie.infrastructure.llm.common.schema.LlmGenerationSchema;
 
 import java.util.Set;
 
 public record LearningExperienceGenerationSchema(
         PromptFragment knowledgeContext,
-        Class<? extends Activity> activityClass,
+        String activityCode,
         ElementalRequirement elementalRequirement,
-        Set<ContentAdjustmentStrategy<?, ?>> contentAdjustmentStrategies
+        Set<PersonalizationRule<?>> personalizationRules
 ) implements LlmGenerationSchema {
+
+    public LearningExperienceGenerationSchema(PromptFragment knowledgeContext,
+                                              Class<? extends Activity> activityClass,
+                                              ElementalRequirement elementalRequirement,
+                                              Set<PersonalizationRule<?>> personalizationRules) {
+        this(knowledgeContext, activityClass.getSimpleName(), elementalRequirement, personalizationRules);
+    }
 }
