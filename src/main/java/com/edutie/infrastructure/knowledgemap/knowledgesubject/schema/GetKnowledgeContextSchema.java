8b9d11b6085bc9eab5df636dcf91bb07c6795119
sukerberk1 <szymon.l3232@gmail.com>
2025-03-27 22:43:51 +0100

    Change GetKnowledgeContextSchema

--- src/main/java/com/edutie/infrastructure/knowledgemap/knowledgesubject/schema/GetKnowledgeContextSchema.java
+++ src/main/java/com/edutie/infrastructure/knowledgemap/knowledgesubject/schema/GetKnowledgeContextSchema.java
@@ -1,10 +1,10 @@
 package com.edutie.infrastructure.knowledgemap.knowledgesubject.schema;
 
-import com.edutie.domain.core.common.generationprompt.PromptFragment;
+import com.edutie.domain.core.education.elementalrequirement.ElementalRequirement;
 import com.edutie.domain.core.education.learningsubject.entities.KnowledgeOrigin;
 
 public record GetKnowledgeContextSchema(
         KnowledgeOrigin knowledgeOrigin,
-        PromptFragment studentObjective
+        ElementalRequirement elementalRequirement
 ) {
 }
