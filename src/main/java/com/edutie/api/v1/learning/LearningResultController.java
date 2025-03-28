a25b2527864e994ed1806c8bf7417812be64e000
sukerberk1 <szymon.l3232@gmail.com>
2025-03-28 19:48:08 +0100

    Add REST endpoint

--- src/main/java/com/edutie/api/v1/learning/LearningResultController.java
+++ src/main/java/com/edutie/api/v1/learning/LearningResultController.java
@@ -2,11 +2,13 @@ package com.edutie.api.v1.learning;
 
 import com.edutie.api.common.ApiResult;
 import com.edutie.api.common.GenericRequestHandler;
+import com.edutie.application.learning.learningresult.CreateLearningResultCommandHandler;
 import com.edutie.application.learning.learningresult.GetLearningResultByIdQueryHandler;
+import com.edutie.application.learning.learningresult.command.CreateLearningResultCommand;
 import com.edutie.application.learning.learningresult.query.GetLearningResultByIdQuery;
 import com.edutie.domain.core.learning.learningresult.LearningResult;
+import com.edutie.domain.core.learning.learningresult.entities.submission.SimpleProblemActivitySolutionSubmission;
 import com.edutie.domain.core.learning.learningresult.identities.LearningResultId;
-import com.edutie.domain.core.learning.learningresult.entities.submission.base.SolutionSubmissionBase;
 import com.edutie.infrastructure.authorization.student.StudentAuthorization;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 public class LearningResultController {
     private final StudentAuthorization studentAuthorization;
     private final GetLearningResultByIdQueryHandler getLearningResultByIdQueryHandler;
+    private final CreateLearningResultCommandHandler createLearningResultCommandHandler;
 
     @GetMapping("/{learningResultId}")
     @Operation(description = "Retrieves a learning result by its identifier")
@@ -34,4 +37,14 @@ public class LearningResultController {
                 );
     }
 
+    //todo: consider adding simple problem activity solution details entity
+    @PostMapping("/create/simple-problem-activity")
+    @Operation(description = "Creates a learning result using a simple problem activity solution submission")
+    public ResponseEntity<ApiResult<LearningResult<SimpleProblemActivitySolutionSubmission>>> createSimpleProblemActivityLearningResult(Authentication authentication,
+                                                                                                                                        @RequestBody CreateLearningResultCommand<SimpleProblemActivitySolutionSubmission> command) {
+        return new GenericRequestHandler<LearningResult<SimpleProblemActivitySolutionSubmission>>()
+                .authenticate(authentication)
+                .authorize(studentAuthorization)
+                .handle((userId) -> createLearningResultCommandHandler.handle(command));
+    }
 }
--- src/main/java/com/edutie/domain/core/learning/learningresult/entities/submission/SimpleProblemActivitySolutionSubmission.java
+++ src/main/java/com/edutie/domain/core/learning/learningresult/entities/submission/SimpleProblemActivitySolutionSubmission.java
@@ -3,10 +3,12 @@ package com.edutie.domain.core.learning.learningresult.entities.submission;
 import com.edutie.domain.core.learning.learningresult.entities.submission.base.RememberingActivitySolutionSubmission;
 import com.edutie.domain.core.learning.learningresult.entities.submission.base.SolutionSubmissionBase;
 import com.edutie.domain.core.learning.learningresult.entities.submission.common.ActivitySolutionParagraph;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +18,11 @@ import java.util.List;
  */
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 @Entity
+@Getter
 public class SimpleProblemActivitySolutionSubmission
         extends SolutionSubmissionBase implements RememberingActivitySolutionSubmission {
     @OneToMany(targetEntity = ActivitySolutionParagraph.class, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JsonProperty
     private List<ActivitySolutionParagraph> solutionParagraphs = new ArrayList<>();
 
     public SimpleProblemActivitySolutionSubmission() {
